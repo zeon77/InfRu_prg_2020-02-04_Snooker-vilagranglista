b272e5e53bf3076bd954d3b5702e433f4eb24104
zeon77 <zeon77@gmail.com>
2021-06-17 12:09:23 +0200

    5. feladat

--- Snooker/Program.cs
+++ Snooker/Program.cs
@@ -21,6 +21,26 @@ namespace Snooker
             //4. feladat
             Console.WriteLine($"4. feladat: A versenyzők átlagosan {Players.Average(x => x.Income):0.00} fontot kerestek.");
 
+            //5. feladat
+            //Rossz megoldás, a MAX-ot minden ciklusban újraszámolja... O(n^2)
+            //SnookerPlayer BestChinesePlayer =
+            //    Players
+            //    .Where(x => x.Country == "Kína").ToList()
+            //    .Single(x => x.Income == Players.Where(y => y.Country == "Kína").Max(y => y.Income));
+
+            //Jobb, de még mindig 3 sor
+            //List<SnookerPlayer> ChinesePlayers = Players.Where(x => x.Country == "Kína").ToList();
+            //double maxChineseIncome = ChinesePlayers.Max(x => x.Income);
+            //SnookerPlayer BestChinesePlayer = ChinesePlayers.Single(x => x.Income == maxChineseIncome);
+
+            //Leg?rövidebb: Max() helyett OrderBy() + Last()/First()
+            SnookerPlayer BestChinesePlayer = Players.Where(x => x.Country == "Kína").OrderBy(x => x.Income).Last();
+            Console.WriteLine($"5. feladat: A legjobban kereső kínai versenyző:");
+            Console.WriteLine($"\t Helyezés: {BestChinesePlayer.Place}");
+            Console.WriteLine($"\t Név: {BestChinesePlayer.Name}");
+            Console.WriteLine($"\t Ország: {BestChinesePlayer.Country}");
+            Console.WriteLine($"\t Nyeremény összege: {(BestChinesePlayer.Income * 380):n0} Ft");
+
         }
     }
 }
