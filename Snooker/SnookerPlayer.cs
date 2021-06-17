afd7b66ef6daa3ceeb34706221d387dd910a527d
zeon77 <zeon77@gmail.com>
2021-06-17 11:40:04 +0200

    Add project files.

---
+++ Snooker/SnookerPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Snooker
+{
+    class SnookerPlayer
+    {
+        public int Place;
+        public string Name;
+        public string Country;
+        public int Income;
+
+        public SnookerPlayer() { }
+
+        public SnookerPlayer(string sor)
+        {
+            string[] Splitted = sor.Split(';');
+            Place = int.Parse(Splitted[0]);
+            Name = Splitted[1];
+            Country = Splitted[2];
+            Income = int.Parse(Splitted[3]);
+        }
+    }
+}
